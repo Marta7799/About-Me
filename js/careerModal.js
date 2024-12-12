c032299245217b390c4639aa248f9f6bb176ed8c
Mart <marta.nikiel@yahoo.com>
2024-12-12 15:02:29 +0100

    cd

--- js/careerModal.js
+++ js/careerModal.js
@@ -1,12 +1,28 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[career-data-modal-open]'),
-      closeModalBtn: document.querySelector('[career-data-modal-close]'),
-      modal: document.querySelector('[career-data-modal]'),
-    };
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-    function toggleModal() {
-      refs.modal.classList.toggle('modal-is-hidden');
-    }
-  })();
+('use strict');
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[career-data-modal-open]'),
+//       closeModalBtn: document.querySelector('[career-data-modal-close]'),
+//       modal: document.querySelector('[career-data-modal]'),
+//     };
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+//     function toggleModal() {
+//       refs.modal.classList.toggle('modal-is-hidden');
+//     }
+//   })();
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  const careerModalOpenButton = document.querySelector('.career-modal-open');
+  const careerModal = document.querySelector('.career-modal');
+  const careerModalCloseButton = document.querySelector('.career-modal-close');
+  
+  careerModalOpenButton.addEventListener('click', () => {
+    careerModal.classList.remove('modal-is-hidden'); 
+  });
+  
+  careerModalCloseButton.addEventListener('click', () => {
+    careerModal.classList.add('modal-is-hidden'); 
+  });
+});
--- js/educationModal.js
+++ js/educationModal.js
@@ -1,12 +1,28 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[education-data-modal-open]'),
-      closeModalBtn: document.querySelector('[education-data-modal-close]'),
-      modal: document.querySelector('[education-data-modal]'),
-    };
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-    function toggleModal() {
-      refs.modal.classList.toggle('info-is-hidden');
-    }
-  })();
+('use strict');
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector('[education-data-modal-open]'),
+//       closeModalBtn: document.querySelector('[education-data-modal-close]'),
+//       modal: document.querySelector('[education-data-modal]'),
+//     };
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+//     function toggleModal() {
+//       refs.modal.classList.toggle('info-is-hidden');
+//     }
+//   })();
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  const educationModalOpenButton = document.querySelector('.education-modal-open');
+  const educationModal = document.querySelector('.education-modal');
+  const educationModalCloseButton = document.querySelector('.education-modal-close');
+  
+  educationModalOpenButton.addEventListener('click', () => {
+    educationModal.classList.remove('modal-is-hidden'); 
+  });
+  
+  educationModalCloseButton.addEventListener('click', () => {
+    educationModal.classList.add('modal-is-hidden'); 
+  });
+});
